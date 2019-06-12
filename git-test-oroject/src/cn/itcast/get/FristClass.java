2515dba858c0d834dcaeff55827d98fd0e257ab8
lifei <591955434@qq.com>
2019-06-12 19:33:07 +0800

    本地工程调价git

---
+++ git-test-oroject/src/cn/itcast/get/FristClass.java
@@ -0,0 +1,12 @@
+package cn.itcast.get;
+
+public class FristClass {
+
+    public static void main(String[] args) {
+        System.out.println("chishi git");
+        System.out.println("chishi git");
+        System.out.println("chishi git");
+        System.out.println("chishi git");
+        System.out.println("chishi git");
+    }
+}
